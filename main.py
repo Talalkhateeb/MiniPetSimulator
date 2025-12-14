48a4a7cf9f4095661de468c2782b276f0fa7f05e
Tala Alkhateeb <talaalkhateeb04@gmail.com>
2025-12-14 18:19:34 +0300

    add file main and pet

---
+++ main.py
@@ -0,0 +1,4 @@
+from pet import pet
+p = pet("krkar",100)
+print("pet name"+p.name)
+print("pet energy level"+p.energy_level)
---
+++ pet.py
@@ -0,0 +1,5 @@
+class pet:
+    def __init__(self,name,energy_level):
+        self.name=name
+        self.energy_level=energy_level
+ 
